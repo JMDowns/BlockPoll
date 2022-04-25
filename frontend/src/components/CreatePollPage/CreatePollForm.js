c009bc94dd45855e1c0d887a3a6c501b425a7a13
Tristan Gant <tgant2017@outlook.com>
2022-04-25 16:51:20 -0500

    create poll style changed. all required UI components done

--- frontend/src/components/CreatePollPage/CreatePollForm.js
+++ frontend/src/components/CreatePollPage/CreatePollForm.js
@@ -1,23 +1,28 @@
-import React, {useState, useEffect, } from 'react'
+import React, {useState, useEffect, Fragment, } from 'react'
 import axios from 'axios'
 
-import {PollFormContainer, PollFormContent, PollFormHeader, PollForm, QuestionContainer,  PollFormLabel,  } from '../StyledComponents/CreatePollForm.style'; 
-import {StyledTextField, Block, StyledButton, CreatePollPage } from '../StyledComponents/CreatePollForm.style'; 
-
 import Switch from '@mui/material/Switch';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
 import SuccessCreatePoll from './SucessCreatePoll'
 
 
 
 export default function CreatePollForm() {
 
+    // CREATE UNIQUE ID WHEN CONNECTION HAPPENS 
     const [values, setValues] = useState({
         title: "",
         description: "",
-        answerOption1: "",
-        answerOption2: "",
-        answerOption3: "", 
-        private: false, 
+        answerOptions: [{
+            option: ''
+        },{
+            option: ''
+        }],
+        privatePoll: false, 
+        id: 0
     })
 
     const [submitted, setSubmitted] = useState(false)
@@ -42,6 +47,23 @@ export default function CreatePollForm() {
 
         e.preventDefault() 
         setSubmitted(true) 
+        console.log(values)
+    }
+
+    const handleChange = (i, e) => {
+        let newValues = [...values.answerOptions]
+        newValues[i][e.target.id] = e.target.value
+        setValues({...values, answerOptions: newValues})
+    }
+
+    const addFormFields = () => {
+        setValues({ ...values, answerOptions: [...values.answerOptions, { option: '' }] }) 
+    }
+
+    const removeFormFields = (i) => {
+        const newValues = [...values.answerOptions];
+        newValues.splice(i, 1);
+        setValues({ ...values, answerOptions: newValues })
     }
 
     const handleTitleChange = (e) => {
@@ -52,104 +74,110 @@ export default function CreatePollForm() {
         setValues({...values, description: e.target.value})
     }
 
-    const handleOption1Change = (e) => {
-        setValues({...values, answerOption1: e.target.value})
-    }
-
-    const handleOption2Change = (e) => {
-        setValues({...values, answerOption2: e.target.value})
-    }
-
-    const handleOption3Change = (e) => {
-        setValues({...values, answerOption3: e.target.value})
-    }
-
     const handlePrivateChange = () => {
-        if(values.private === false){
-            setValues({...values, private: true})
+        if(values.privatePoll === false){
+            setValues({...values, privatePoll: true})
             return
         }
-        setValues({...values, private: false})
+        setValues({...values, privatePoll: false})
     }
 
     return (
-        <div>
-        <CreatePollPage className="WHOLEPAGE">
-            {submitted ? <SuccessCreatePoll sub={setSubmitted} />  : //PUT THE SUCCESSCREATEPOLL HERE
-            <PollFormContainer name="pollcontainer" height="105%">
-                <PollFormHeader name="header">
-                    Create Poll
-                </PollFormHeader>
-                <PollFormContent name="content">
-                    <PollForm name="form" onSubmit={ handleSubmit }>
-                        <QuestionContainer name="QuestionContainer">
-                            <Block marginBottom="5%">
-                                <PollFormLabel htmlFor="Title">Title</PollFormLabel>
-                                    <StyledTextField required={true} 
-                                                    value={values.title}
-                                                    onChange={handleTitleChange}
-                                                    id="Title" 
-                                                    label="Title" 
-                                                    variant="outlined"
-                                                    validation="This Field is Required" />
-                            </Block>
-
-                            <Block marginBottom="5%">
-                                <PollFormLabel htmlFor="Description">Description</PollFormLabel>
-                                <StyledTextField required={false} 
-                                                value={values.description}
-                                                onChange={handleDescriptionChange}
-                                                id="Description" 
-                                                label="Description" 
-                                                variant="outlined"
-                                                validation="This Field is Required" />
-                            </Block>
-
-                            <Block marginBottom="5%">
-                                <PollFormLabel htmlFor="Answer1">Answer Options</PollFormLabel>
-                                <StyledTextField required={true} 
-                                                value={values.answerOption1}
-                                                onChange={handleOption1Change}
-                                                id="Answer1" 
-                                                label="Option 1" 
-                                                variant="outlined"
-                                                validation="This Field is Required" />
-
-                                <StyledTextField required={true} 
-                                                value={values.answerOption2}
-                                                onChange={handleOption2Change}
-                                                id="Answer2" 
-                                                label="Option 2" 
-                                                variant="outlined"
-                                                validation="This Field is Required" />
-                                <StyledTextField required={false} 
-                                                value={values.answerOption3}
-                                                onChange={handleOption3Change}
-                                                id="Answer3" 
-                                                label="Option 3" 
-                                                variant="outlined"
-                                                validation="This Field is Required" />
-                                </Block>
-                                <Block marginBottom="5%">
-                                    <PollFormLabel value={values.private} id="Private">Private (via link only)</PollFormLabel>
-                                    <Switch
-                                        onChange={handlePrivateChange}
-                                        inputProps={{ 'aria-label': 'controlled' }}
+        <div className="create-poll-container">
+            {submitted ? 
+                <SuccessCreatePoll 
+                    title={values.title}
+                    description={values.description}
+                    privatePoll={values.privatePoll}
+                    answerOptions={values.answerOptions}
+                    id={values.id}
+                    sub={setSubmitted} 
+                    
+                />  :
+                <Fragment>
+                <form name="create-poll" onSubmit={handleSubmit}>
+                <Grid container>
+                    <Grid item xs={6}>
+                        <h1>Create Poll</h1>
+                    </Grid>
+                    <Grid item xs={6}>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <div className="question-container">
+                            <label>Title</label>
+                            <TextField
+                                required={true}
+                                value={values.title}
+                                onChange={handleTitleChange}
+                                id="Title" 
+                                placeholder="Title" 
+                                variant="outlined"
+                                validation="This Field is Required" 
+                                sx={{ backgroundColor: '#F8FAFC' }}
+                            />
+                            <label>Description</label>
+                            <TextareaAutosize
+                                value={values.description}
+                                onChange={handleDescriptionChange}
+                                id="Description" 
+                                label="Description" 
+                                placeholder="Description"
+                                variant="outlined"
+                                minRows={3}
+                                validation="This Field is Required"
+                                sx={{ backgroundColor: '#F8FAFC' }} 
+                            />
+                            <label>Private</label>
+                                <Switch
+                                    onChange={handlePrivateChange}
+                                    inputProps={{ 'aria-label': 'controlled' }}
+                            />
+                            <Button  
+                                type="submit" 
+                                variant="contained">Create Poll
+                            </Button>
+                        </div>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <div className="option-container">
+                            <label>Answer Options</label>
+                            {values.answerOptions.map((element, index) => (
+                                <Fragment key={index}>
+                                    <TextField
+                                        required={true}
+                                        value={element.option}
+                                        onChange={e => handleChange(index, e)}
+                                        id="option"
+                                        placeholder={`Option ${index+1}`} 
+                                        variant="outlined"
+                                        validation="This Field is Required"
+                                        sx={{ backgroundColor: '#F8FAFC' }}
                                     />
-                                </Block>
-
-                        </QuestionContainer>
-                        <Block display="flex" 
-                                    alignItems="center" 
-                                    justifyContent="center"
-                                    height="10%">
-                            <StyledButton type="submit" variant="contained">Create Poll</StyledButton>
-                        </Block>
-                    </PollForm>
-                </PollFormContent>
-            </PollFormContainer>
+                                </Fragment>
+                            ))}
+                            {values.answerOptions.length < 5 ?
+                                <Button 
+                                    variant="text"
+                                    onClick={() => addFormFields()}
+                                >
+                                    Add Option
+                                </Button>
+                            : null}
+                            {values.answerOptions.length > 2 ? 
+                                <Button 
+                                    variant="text"
+                                    color="error"
+                                    onClick={() => removeFormFields()}
+                                >
+                                Remove Option
+                                </Button>                        
+                            : null}
+                        </div>
+                    </Grid>
+                </Grid>
+                </form>
+                </Fragment>
             }
-        </CreatePollPage>
         </div>
     )
 }
--- frontend/src/routes/ViewPolls.jsx
+++ frontend/src/routes/ViewPolls.jsx
@@ -2,6 +2,8 @@ import React, { useState, Fragment, } from 'react'
 import Button from '@mui/material/Button';
 import { GlobalStyles, } from '../components/StyledComponents/GlobalStyles.style';
 import Polls from '../components/ViewPolls/Polls'
+import PublicIcon from '@mui/icons-material/Public';
+import PublicOffIcon from '@mui/icons-material/PublicOff';
 
 
 export default function ViewPolls() {
@@ -16,10 +18,25 @@ export default function ViewPolls() {
             <h1>View Polls</h1>
             <ul>
                 <li>
-                    <Button variant="contained" size="large" fullWidth={true} onClick={handlePublic}>Public</Button>
+                    <Button 
+                        variant="contained" 
+                        size="large" 
+                        fullWidth={true} 
+                        onClick={handlePublic}
+                        endIcon={<PublicIcon />}
+                    >
+                        Public
+                    </Button>
                 </li>
                 <li>
-                    <Button variant="contained" size="large" fullWidth={true} onClick={handlePrivate}>Private</Button>
+                    <Button 
+                    variant="contained" 
+                    size="large" fullWidth={true} 
+                    onClick={handlePrivate}
+                    endIcon={<PublicOffIcon />}
+                >
+                    Private
+                </Button>
                 </li>
             </ul>
                 {status && <Polls status={status}/>}
